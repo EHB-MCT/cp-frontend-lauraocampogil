5c0a465a43bf35fee9cb520b05fa634a2cc1cf39
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-04-28 15:16:26 +0200

    Created footer wihtout style

--- course-project-parallax/src/App.jsx
+++ course-project-parallax/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Footer />
 		</>
 	);
 }
---
+++ course-project-parallax/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+	return (
+		<footer>
+			<h3>About Us</h3>
+			<p>Welcome to a world where fairy tales scroll with depth. Through parallax magic, we bring front-end stories to life — one layer at a time. ✨</p>
+			<h3>Links</h3>
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">Projects</a>
+				</li>
+				<li>
+					<a href="#">Making Of</a>
+				</li>
+			</ul>
+		</footer>
+	);
+}
+export default Footer;
