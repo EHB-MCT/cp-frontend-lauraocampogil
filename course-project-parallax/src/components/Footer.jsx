08dddab27a9a2b6322dcd40629750ce8933781eb
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-04-28 18:19:25 +0200

    logo add and searcbar component created

--- course-project-parallax/src/components/Footer.jsx
+++ course-project-parallax/src/components/Footer.jsx
@@ -1,6 +1,10 @@
+import Logo from "../assets/Images-web/Logo_ErWasEens.svg";
 function Footer() {
 	return (
 		<footer>
+			<div className="logo-footer">
+				<img src={Logo}></img>
+			</div>
 			<h3>About Us</h3>
 			<p>Welcome to a world where fairy tales scroll with depth. Through parallax magic, we bring front-end stories to life — one layer at a time. ✨</p>
 			<h3>Links</h3>
--- course-project-parallax/src/components/Header.jsx
+++ course-project-parallax/src/components/Header.jsx
@@ -1,20 +1,24 @@
+import Logo from "../assets/Images-web/Logo_ErWasEens.svg";
 function Header() {
 	return (
 		<header>
-			<h1>Parallax Website</h1>
 			<nav>
-				<ul>
-					<li>
-						<a href="#">Home</a>
-					</li>
-					<li>
-						<a href="#">Projects</a>
-					</li>
-					<li>
-						<a href="#">Making Of</a>
-					</li>
-				</ul>
-				<hr></hr>
+				<div className="logo">
+					<img src={Logo}></img>
+				</div>
+				<div className="menu">
+					<ul className="menu-item">
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">Projects</a>
+						</li>
+						<li>
+							<a href="#">Making Of</a>
+						</li>
+					</ul>
+				</div>
 			</nav>
 		</header>
 	);
