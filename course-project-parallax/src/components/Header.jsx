821706ed0b5a82e307f2d0b183754f732c49ceb2
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-04-28 11:56:32 +0200

    Header created without logo and style

--- course-project-parallax/src/components/Header.jsx
+++ course-project-parallax/src/components/Header.jsx
@@ -2,6 +2,20 @@ function Header() {
 	return (
 		<header>
 			<h1>Parallax Website</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Projects</a>
+					</li>
+					<li>
+						<a href="#">Making Of</a>
+					</li>
+				</ul>
+				<hr></hr>
+			</nav>
 		</header>
 	);
 }
