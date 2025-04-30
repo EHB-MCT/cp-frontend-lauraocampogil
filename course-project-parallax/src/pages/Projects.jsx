a5243221288aedf5bc0994c12fc1460d6585a229
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-04-30 08:12:26 +0200

    Projects page is created

--- course-project-parallax/src/pages/Projects.jsx
+++ course-project-parallax/src/pages/Projects.jsx
@@ -1,8 +1,30 @@
+import Footer from "../components/Footer.jsx";
+import FairydustCursor from "../components/FairydustCursor.jsx";
+import Fairytales from "../fairytale-data.json";
 function Projects() {
 	return (
-		<div>
-			<h1>Projects</h1>
-		</div>
+		<>
+			<div className="hero-title">
+				<h1>PROJECTS</h1>
+			</div>
+			<div className="fairy-tales-wrapper">
+				<div className="fairy-tales-grid">
+					{Fairytales.map((fairytale) => {
+						return (
+							<div className="fairy-tale" key={fairytale.id}>
+								<img src={fairytale.image} alt="" />
+								<div className="fairy-tale-text">
+									<h2>{fairytale.title}</h2>
+									<p>{fairytale.name}</p>
+									<button className="fairy-tale-button">i</button>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+			<Footer />;
+		</>
 	);
 }
 
