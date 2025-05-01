492fa3e2b812c79570c56214740d4f3c1ffc072d
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-05-01 21:05:49 +0200

    Making-of page is finished-yt video is integrated with npm link

--- course-project-parallax/src/pages/MakingOf.jsx
+++ course-project-parallax/src/pages/MakingOf.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import Banner from "../assets/Images-web/Banner-peterpan.png";
 function MakingOf() {
 	return (
@@ -12,14 +15,23 @@ function MakingOf() {
 				<div className="explainvideo-container">
 					<div className="video-section">
 						<h2>Explainer Video</h2>
+						<div className="video-yt">
+							<LiteYouTubeEmbed id="nDM499ppEWk" title="Peter Pan Explainer Video" poster="maxresdefault" />
+						</div>
 					</div>
-					<div className="description-container">
+					<div className="description-section">
 						<h2>Description</h2>
-					</div>
-					<div className="visitwebsite-btn">
-						<a href="/parallax">
-							<button className="visitwebsite-button">Visit Website</button>
-						</a>
+						<p>
+							Embark on a magical journey through Peter Pan's enchanting world in this immersive parallax experience.From the Darling children's bedroom to the mysterious Neverland island, where adventure awaits at every scroll. Encounter the
+							spirited Lost Boys, face Captain Hook's menacing pirates aboard the Jolly Roger, and follow Tinkerbell's cursor and magic fee through lush forests and crystal lagoons. Don't forget to activate your sound for the full enchantment—the
+							lost boys and Peter Pan await!
+						</p>
+
+						<div className="visitwebsite-btn">
+							<a href="/parallax">
+								<button className="visitwebsite-button">Visit Website</button>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
