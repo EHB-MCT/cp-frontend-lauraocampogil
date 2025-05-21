5b3db0500c998549545cffe765fd14c0c5d3e282
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-05-21 15:59:10 +0200

    for the firstpageparallax changed pages to components, delete basename in hashrouter and index from firstpageparallax in app.jsx

--- course-project-parallax/src/App.jsx
+++ course-project-parallax/src/App.jsx
@@ -5,21 +5,19 @@ import Home from "./pages/Home.jsx";
 import Projects from "./pages/Projects.jsx";
 import MakingOf from "./pages/MakingOf.jsx";
 import Parallax from "./pages/Parallax.jsx";
-import FirstpageParallax from "./pages/FirstpageParallax.jsx";
+import FirstpageParallax from "./components/FirstpageParallax.jsx";
 
 function App() {
 	return (
 		<>
-			<HashRouter basename="/cp-frontend-lauraocampogil/">
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
 						<Route path="projects" element={<Projects />} />
 						<Route path="makingof" element={<MakingOf />} />
 					</Route>
-					<Route path="/firstpageparallax">
-						<Route index element={<FirstpageParallax />} />
-					</Route>
+					<Route path="/firstpageparallax" element={<FirstpageParallax />} />
 					<Route path="/parallax" element={<Parallax />} />
 				</Routes>
 			</HashRouter>
